6a62d1ef5838a6ed9d6e8c916e8fd19c2058d02e
theoliverlear <sigwartholiver2@gmail.com>
2025-08-07 09:29:15 -0500

    Refactor Solution.java to extract methods for handling nested and repeated string patterns for problem three hundred ninety-four

--- Problem-0394-Decode-String/Problem-0394-Optimized/java/Solution.java
+++ Problem-0394-Decode-String/Problem-0394-Optimized/java/Solution.java
@@ -8,30 +8,38 @@ public class Solution {
         }
         final String repeatPatternRegex = "([a-z]*)(\\d+)\\[(\\D+)\\](.*)";
         final String isNestedRegex = ".*\\[.[^\\]]*\\[.*\\].*\\].*";
-        final String findNestedRegex = "\\d+\\[[\\D]*([a-z]*\\d+\\[[a-z]+\\])(.*)";
-        
         if (encodedString.matches(isNestedRegex)) {
-            System.out.println("Is nested");
-            Pattern nestedPattern = Pattern.compile(findNestedRegex);
-            Matcher nestedMatcher = nestedPattern.matcher(encodedString);
-            if (nestedMatcher.find()) {
-                String nestedRepeat = nestedMatcher.group(1);
-                String decodedRepeat = this.decodeString(nestedRepeat);
-                encodedString = encodedString.replace(nestedRepeat, decodedRepeat);
-            }
+            encodedString = getNestedEncodedString(encodedString);
         } else if (encodedString.matches(repeatPatternRegex)) {
-            System.out.println(encodedString);
-            Pattern repeatPattern = Pattern.compile(repeatPatternRegex);
-            Matcher repeatMatcher = repeatPattern.matcher(encodedString);
-            if (repeatMatcher.find()) {
-                String leadingLetters = repeatMatcher.group(1);
-                int multiplier = Integer.parseInt(repeatMatcher.group(2));
-                String repeatContent = repeatMatcher.group(3);
-                String restOfString = repeatMatcher.group(4);
-                encodedString = leadingLetters + repeatContent.repeat(multiplier) + restOfString;
-            }
+            encodedString = getRepeatString(encodedString, repeatPatternRegex);
         }
         return this.decodeString(encodedString);
+    }
+
+    private static String getRepeatString(String encodedString,
+                                          String repeatPatternRegex) {
+        Pattern repeatPattern = Pattern.compile(repeatPatternRegex);
+        Matcher repeatMatcher = repeatPattern.matcher(encodedString);
+        if (repeatMatcher.find()) {
+            String leadingLetters = repeatMatcher.group(1);
+            int multiplier = Integer.parseInt(repeatMatcher.group(2));
+            String repeatContent = repeatMatcher.group(3);
+            String restOfString = repeatMatcher.group(4);
+            encodedString = leadingLetters + repeatContent.repeat(multiplier) + restOfString;
+        }
+        return encodedString;
+    }
+
+    private String getNestedEncodedString(String encodedString) {
+        final String findNestedRegex = "\\d+\\[[\\D]*([a-z]*\\d+\\[[a-z]+\\])(.*)";
+        Pattern nestedPattern = Pattern.compile(findNestedRegex);
+        Matcher nestedMatcher = nestedPattern.matcher(encodedString);
+        if (nestedMatcher.find()) {
+            String nestedRepeat = nestedMatcher.group(1);
+            String decodedRepeat = this.decodeString(nestedRepeat);
+            encodedString = encodedString.replace(nestedRepeat, decodedRepeat);
+        }
+        return encodedString;
     }
 
     private static boolean isDecodedString(String encodedString) {
